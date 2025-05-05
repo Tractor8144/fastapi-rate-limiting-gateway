d24f20b2360233f2f502b1d2b453b0a2898eb9d7
Maharshi <joshimaharshi123@gmail.com>
2025-05-05 22:46:19 +0530

    Replace mock backend with reverse proxy for specified backend_url
    Replace BACKEND_URL with your backend's url and the reverse proxy server will be attached

--- app/main.py
+++ app/main.py
@@ -1,10 +1,31 @@
 from fastapi import FastAPI, Request
 from .middleware.rate_limiter import RateLimiterMiddleware
+from starlette.responses import Response
+import httpx
 
 app = FastAPI()
 app.add_middleware(RateLimiterMiddleware)
 
+BACKEND_URL = 'http://localhost:9000'
 
-@app.get("/test/path")
-async def test_endpoint():
-    return {"message": "allowed"}
+
+@app.api_route("/{full_path:path}", methods=["GET", "POST", "PUT", "DELETE", "PATCH"])
+async def proxy(full_path: str, request: Request):
+    req_body = await request.body()
+    headers = dict(request.headers)
+
+    async with httpx.AsyncClient() as client:
+        backend_response = await client.request(
+            method=request.method,
+            url=f"{BACKEND_URL}/{full_path}",
+            headers=headers,
+            content=req_body,
+            params=request.query_params
+        )
+
+    return Response(
+        content=backend_response.content,
+        status_code=backend_response.status_code,
+        headers=dict(backend_response.headers),
+        media_type=backend_response.headers.get("content-type")
+    )
