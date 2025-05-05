9841883565a80196f62e1d1730301083b3515cc6
Maharshi <joshimaharshi123@gmail.com>
2025-05-05 22:40:22 +0530

    Format/refactor middleware

--- app/middleware/rate_limiter.py
+++ app/middleware/rate_limiter.py
@@ -4,19 +4,20 @@ from starlette.responses import JSONResponse
 from app.services.redis_handler import get_token_bucket
 from app.services.status_enum import StatusType
 
+
 class RateLimiterMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next) -> JSONResponse:
         key = f"rate_limit:{request.client.host}"
         status = get_token_bucket(key, limit=5, refill_rate=1)
-        if status==StatusType.REFUSED:
+        if status == StatusType.REFUSED:
             return JSONResponse(
                 status_code=429,
                 content={'detail': 'Too many requests'}
             )
-        elif status==StatusType.REDIS_ERROR:
+        elif status == StatusType.REDIS_ERROR:
             return JSONResponse(
                 status_code=503,
-                content={'detail':'Redis server error'}
+                content={'detail': 'Redis server error'}
             )
         response = await call_next(request)
         return response
